c94649a2432ad065efb6df8b8b4f105400e3fe54
pieper126 <stijn.bijnen@invoicefinance.nl>
2017-05-15 15:58:56 +0200

    Fixes style in find_company

--- lib/creditsafe/request/find_company.rb
+++ lib/creditsafe/request/find_company.rb
@@ -12,8 +12,10 @@ module Creditsafe
         @company_name = search_criteria[:company_name]
         @city = search_criteria[:city]
         @match_type = search_criteria[:match_type] || 'MatchBeginning'
+        @postal_code = search_criteria[:postal_code]
       end
 
+      # rubocop:disable Metrics/MethodLength
       def message
         search_criteria = {}
 
@@ -31,12 +33,17 @@ module Creditsafe
           "#{Creditsafe::Namespace::DAT}:City" => city
         } unless city.nil?
 
+        search_criteria["#{Creditsafe::Namespace::DAT}:Address"] = {
+          "#{Creditsafe::Namespace::DAT}:PostalCode" => postal_code
+        } unless postal_code.nil?
+
         build_message(search_criteria)
       end
+      # rubocop:enable Metrics/MethodLength
 
       private
 
-      attr_reader :country_code, :registration_number, :city, :company_name
+      attr_reader :country_code, :registration_number, :city, :company_name, :postal_code
 
       def build_message(search_criteria)
         {
@@ -47,15 +54,12 @@ module Creditsafe
         }
       end
 
-      # rubocop:disable Style/CyclomaticComplexity, Metrics/AbcSize
+      # rubocop:disable Metrics/CyclomaticComplexity, Metrics/AbcSize
+      # rubocop:disable Metrics/PerceivedComplexity, Metrics/MethodLength
       def check_search_criteria(search_criteria)
         if search_criteria[:country_code].nil?
           raise ArgumentError, "country_code is a required search criteria"
         end
-
-        #if search_criteria[:country_code] != 'DE' && !search_criteria[:company_name].nil?
-          #raise ArgumentError, "company name search is only possible for German searches"
-        #end
 
         unless only_registration_number_or_company_name_provided?(search_criteria)
           raise ArgumentError, "registration_number or company_name (not both) are " \
@@ -65,8 +69,13 @@ module Creditsafe
         if search_criteria[:city] && search_criteria[:country_code] != 'DE'
           raise ArgumentError, "city is only supported for German searches"
         end
+
+        if search_criteria[:postal_code] && search_criteria[:country_code] != 'DE'
+          raise ArgumentError, "Postal code is only supported for German searches"
+        end
       end
-      # rubocop:enable Style/CyclomaticComplexity, Metrics/AbcSize
+      # rubocop:enable Metrics/CyclomaticComplexity, Metrics/AbcSize
+      # rubocop:enable Metrics/PerceivedComplexity, Metrics/MethodLength
 
       def only_registration_number_or_company_name_provided?(search_criteria)
         search_criteria[:registration_number].nil? ^ search_criteria[:company_name].nil?
